2cbde0c442a1651948e99d49516bfdac6bfe49e4
shahbozurinov <102744266+shahbozurinov@users.noreply.github.com>
2022-04-10 17:50:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let hamburger = document.querySelector('.hamburger');
+let navMenu = document.querySelector('.uls');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+
+
+
+let pl_1 = document.querySelector('.pl-1');
+let pl_2 = document.querySelector('.pl-2');
+let pl_3 = document.querySelector('.pl-3');
+let pl_4 = document.querySelector('.pl-4');
+let pl_5 = document.querySelector('.pl-5');
+let pl_6 = document.querySelector('.pl-6');
+let pl_7 = document.querySelector('.pl-7');
+let pl_8 = document.querySelector('.pl-8');
+let pl_9 = document.querySelector('.pl-9');
+let pl_10 = document.querySelector('.pl-10');
+let pl_11 = document.querySelector('.pl-11');
+let pl_12 = document.querySelector('.pl-12');
+let pl_13 = document.querySelector('.pl-13');
+
+pl_1.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_2.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_3.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_4.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_5.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_6.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_7.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_8.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_9.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_10.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_11.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_12.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
+
+pl_13.addEventListener('click', function() {
+    this.classList.toggle('deg') 
+})
